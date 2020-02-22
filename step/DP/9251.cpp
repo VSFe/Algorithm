bf56aa6a93e140f032e9944ab88f5cfa0426953d
VSFe <klm03025@gmail.com>
2020-02-22 20:43:23 +0900

    200222 으악 시발 제발 이메일 써

--- step/DP/9251.cpp
+++ step/DP/9251.cpp
@@ -6,7 +6,7 @@ LCS(Longest Common Subsequence, 최장 공통 부분 수열)문제는 두 수열
 모두의 부분 수열이 되는 수열 중 가장 긴 것을 찾는 문제이다.
 예를 들어, ACAYKP와 CAPCAK의 LCS는 ACAK가 된다.
 Comment: 공간복잡도를 조금 감수합시다...
-*/
+*/ 
 
 #include <stdio.h>
 #include <iostream>
